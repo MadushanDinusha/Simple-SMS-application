9e1dd1bd1bab3d559420fd2e297e4954c5bbabbc
MadushanDinusha <djabc.pwc@mail.com>
2019-05-02 14:01:08 +0530

    New: pom.xml file added

---
+++ src/test/java/com/sms/application/smsapplication/SmsapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sms.application.smsapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmsapplicationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
